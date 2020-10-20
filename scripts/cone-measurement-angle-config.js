146c5da548a019a5c0d3daaebd2a9e9aec606415
thre-z <73196709+thre-z@users.noreply.github.com>
2020-10-20 16:01:50 -0400

    Update cone-measurement-angle-config.js

--- scripts/cone-measurement-angle-config.js
+++ scripts/cone-measurement-angle-config.js
@@ -1,1 +1,17 @@
-CONFIG.MeasuredTemplate.defaults.angle = 90;
+Hooks.on("init", () => {
+  game.settings.register("cone-measurement-angle", "coneAngle", {
+    name: "Cone Measurement Angle",
+    hint: "Default angle for cone measurement tool.",
+    scope: "world",
+    config: true,
+    type: Integer,
+    default: 90
+  })
+})
+
+class DDImporter extends Application
+{
+
+  CONFIG.MeasuredTemplate.defaults.angle = 90;
+
+}
